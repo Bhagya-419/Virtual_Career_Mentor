c3d3ab4d82f071c0649e59ad05b65f883c0213aa
Bhagya-419 <blakshminukala@gmail.com>
2026-08-03 23:37:21 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import { toast } from "react-toastify"
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: process.env.REACT_APP_API_URL
 })
 console.log("API BASE URL:", API.defaults.baseURL)
 // Attach token automatically to every request
